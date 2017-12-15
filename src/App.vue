6d252b6f3ba4ca084c507d0383fc091ffd4b04b3
yuzhilin4575 <1297278412@qq.com>
2017-12-15 11:14:49 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Dheader></Dheader>
+    <transition name="fade">
+      <keep-alive exclude="moviesDetail">
+        <router-view></router-view>
+      </keep-alive>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'hello vue'
+    }
+  },
+  components: {
+    'Dheader': (resolve) => {
+      require(['./header'], resolve)
+    }
+  }
+}
+</script>
+
+<style lang="less" rel="stylesheet/less">
+
+  *{
+    margin: 0;
+    padding: 0;
+    font-family: '微软雅黑';
+  }
+</style>
